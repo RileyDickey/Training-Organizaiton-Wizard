011aa3ba2d23846a11d69f83f5527c5d2297f4f9
Riley Dickey <105519456+RileyDickey@users.noreply.github.com>
2022-09-15 10:52:23 -0600

    Add files via upload

---
+++ TrainingOrgCertAddRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests
+{
+    public class TrainingOrgCertAddRequest
+    {
+        [Required]
+        [Range(1, Int32.MaxValue)]
+        public int OrganizationId { get; set; }
+
+        [Required]
+        public List<string> BatchOrgCerts { get; set; }
+
+    }
+}
